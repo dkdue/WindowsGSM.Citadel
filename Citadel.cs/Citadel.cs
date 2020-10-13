d55294ef1ab8d2431c4b31cfcea44c8b7b38aa66
kessef <dkdue@hotmail.com>
2020-10-13 12:18:39 +0200

    Add files via upload

--- Citadel.cs/Citadel.cs
+++ Citadel.cs/Citadel.cs
@@ -44,7 +44,7 @@ namespace WindowsGSM.Plugins
         // - Game server default values
         public string Port = "7777"; // Default port
         public string QueryPort = "27015"; // Default query port
-        public string Defaultmap = "TheWorld"; // Default map name
+        public string Defaultmap = "Ignus"; // Default map name
         public string Maxplayers = "75"; // Default maxplayers
         public string Additional = ""; // Additional server start parameter
 
