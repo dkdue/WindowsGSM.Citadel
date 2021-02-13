ba1aba07d10af1af65a77bc98d26507bdac7c5c2
kessef <dkdue@hotmail.com>
2021-02-13 19:52:33 +0100

    Add files via upload

--- Citadel.cs/Citadel.cs
+++ Citadel.cs/Citadel.cs
@@ -34,7 +34,7 @@ namespace WindowsGSM.Plugins
 
 
         // - Game server Fixed variables
-        public override string StartPath => @"Citadel\Binaries\Win64\CitadelServer.exe"; // Game server start path
+        public override string StartPath => @"Citadel\Binaries\Win64\CitadelServer-Win64-Shipping.exe"; // Game server start path
         public string FullName = "Citadel Dedicated Server"; // Game server FullName
         public bool AllowsEmbedConsole = true;  // Does this server support output redirect?
         public int PortIncrements = 1; // This tells WindowsGSM how many ports should skip after installation
